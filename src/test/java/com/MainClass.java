982dd60c88d86fa7d0f4e64c8afef35473090316
Ugur Yilmazer <kobra667788@gmail.com>
2021-06-22 23:15:00 +0300

    ugur work2 inside main class

--- src/test/java/com/MainClass.java
+++ src/test/java/com/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("This is the first team lead code");
+        System.out.println("This is  new code from uguryilmazer");
     }
 
 
